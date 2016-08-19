6b144b59d1f047eb43c6e19987a6eab3a945ae69
Emma Carrigan <emmeridge@hotmail.com>
2016-08-19 21:28:22 +0100

    going well so far

--- src/main.c
+++ src/main.c
@@ -1,5 +1,11 @@
-#include <winsock.h>
-#include "stdio.h"
+// started 19/08/16
+// using http://beej.us/guide/bgnet/output/html/singlepage/bgnet.html
+
+#include <stdio.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+
+#define PORT "3490"
 
 int main() {
 	WSAData wsaData;
@@ -8,4 +14,13 @@ int main() {
 		fprintf(stderr, "WSAStartup failed.\n");
 		exit(1);
 	}
+
+	int sockfd, new_fd;  // listen on sock_fd, new connection on new_fd
+    struct addrinfo hints, *servinfo, *p;
+    struct sockaddr_storage their_addr; // connector's address information
+    socklen_t sin_size;
+    struct sigaction sa; // WHY DOESN'T THIS WORK I DON'T UNDERSTAND NETWORKING AT ALL I AM DONE
+    int yes=1;
+    char s[INET6_ADDRSTRLEN];
+    int rv;
 }
